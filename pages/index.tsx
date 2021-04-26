fc0b4c3046befefb73f9af485711b013d801c3da
Yaser Farhan <yaser.syed@gmail.com>
2021-04-26 05:14:04 -0700

    convert to TS add some basic styling

---
+++ pages/index.tsx
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+import { chakra } from '@chakra-ui/react';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <p>Comming soon</p>
+          <chakra.button
+            px="3"
+            py="2"
+            bg="green.200"
+            rounded="md"
+            _hover={{ bg: 'green.300' }}
+          >
+            Click me
+          </chakra.button>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}
